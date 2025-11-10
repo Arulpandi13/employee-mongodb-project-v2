4d2a996a2cdcd9f56b1d5f96336350f203a6201b
Arul-Tech <arulpandi9629@gmail.com>
2025-11-10 14:24:10 +0530

    Updated project files

--- src/main/java/com/example/TechLambdas/TechLambdasApplication.java
+++ src/main/java/com/example/TechLambdas/TechLambdasApplication.java
@@ -8,7 +8,7 @@ public class TechLambdasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechLambdasApplication.class, args);
-		System.out.println("Mythilli oru lusu");
+		System.out.println("Nirmal oru kirukan");
 	}
 
 }
